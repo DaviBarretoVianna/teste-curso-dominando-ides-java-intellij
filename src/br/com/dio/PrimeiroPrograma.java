2452a95f6a22066f3d7e40240fa40b0d4cc487e4
DaviBarretoVianna <davibarretovianna@gmail.com>
2021-08-24 15:37:33 -0300

    First commit

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -1,15 +1,46 @@
-package br.com.dio;
+        package br.com.dio;
 
-import br.com.dio.model.gato;
+        import br.com.dio.model.gato;
 
-public class PrimeiroPrograma {
-    public static void main(String[] args) {
-        gato gato = new gato();
-        System.out.println(gato);
+        public class PrimeiroPrograma {
+            public static void main(String[] args) {
+                gato gato = new gato();
+                System.out.println(gato);
 
+                Livro livro = new Livro("Novo Livro", 100);
+                System.out.println(livro);
+            }
+}
+class Livro {
+    private String nome;
+    private Integer paginas;
 
-        /*int a = 5;
-        int b = 3;
-        System.out.println("Hello world!" + (a+b));*/
+    public Livro(String nome, Integer paginas) {
+        this.nome = nome;
+        this.paginas = paginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(Integer paginas) {
+        this.paginas = paginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+        "nome='" + nome + '\'' +
+        ", paginas=" + paginas +
+        '}';
     }
 }
